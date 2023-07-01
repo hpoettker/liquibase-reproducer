0476a63cad857cc51a0ecf8341d411586a208c28
Henning Poettker <Henning.Poettker@c24.de>
2023-07-01 19:02:40 +0200

    init commit

---
+++ src/main/java/com/example/liquibasereproducer/LiquibaseReproducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasereproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseReproducerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiquibaseReproducerApplication.class, args);
+  }
+
+}
